48d642088cb32cba52aeb263297626d73478cac5
abimanyusatriaa <adji3890@gmail.com>
2024-05-28 10:32:15 +0700

    no overiding

---
+++ noOveriding/noOveriding.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+//class parent
+//tambahan final sesudah nama class
+//untuk mencegah adanya overiding
+
+class baseClass
+{
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
